cbac407e7e80a49478bdbd91ead7ef3c10642840
kingtamaaj <kingtamaaj@gmail.com>
2019-06-21 14:15:39 -0400

    mvp

--- api/server.js
+++ api/server.js
@@ -5,11 +5,27 @@ const projectRouter = require('../projectRouter')
 const server = express();
 
 
+
 // Router
 server.use(express.json());
 server.use('/api/projects', projectRouter);
 server.use('/api/actions', actionRouter);
 
 
+// custom middleware
+server.use(insertName);
+
+
+server.get('/', (req, res) => {
+    const name = (req.name) ? ` ${req.name} ` : '';
+
+    res.send(`<h1> Lambbda ${name} WebAPI Challenge </h1>`);
+});
+
+
+function insertName (req, res, next) {
+    req.name = req.name || 'Tamaaj';
+    next();
+}
 
 module.exports = server;
